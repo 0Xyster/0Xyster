bcadb0387803ffb7f2119e504014de9ef565cb47
0Xyster <xyster248@gmail.com>
2025-05-11 22:47:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+
+var _0x1a2b=['VIN@518GOD'];function checkCode(){var _0xabc=document.getElementById("codeInput").value;if(_0xabc===_0x1a2b[0]){document.getElementById("protectedContent").classList.remove("hidden");}else{alert("❌ Wrong code. Try again.");}}
